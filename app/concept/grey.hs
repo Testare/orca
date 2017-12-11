1923840a1bd0e54f2366b4494fa80829f55d2a22
testare <testare.i9z@gmail.com>
2017-12-11 12:44:54 -0700

    Symbol identification

--- app/concept/grey.hs
+++ app/concept/grey.hs
@@ -9,6 +9,8 @@ import Graphics.Image.Processing.Filter(sobelOperator,prewittOperator)
 import Graphics.Image.Processing(crop)
 import Graphics.Image.Interface
 import Data.Int(Int64)
+import Data.List(sortOn, intercalate)
+import Data.Ratio
 import qualified Data.Vector.Storable as V
 import Data.Monoid(mconcat)
 import Control.Monad(liftM)
@@ -25,21 +27,64 @@ ops :: [(String, IO ())]
 ops = [ ("Grey thresholding", threshMain)
       , ("Prewitt operator", prewittMain) 
       , ("Test battery", runFullTests)
-      , ("Test splitting", splitTest)
+      , ("Test splitting", smallSplitTest)
+      , ("Test big splitting", splitTest False)
       , ("Test second splitting", testSplitting2)
-      , ("Test filtered splitting", testFilteredSplitting)
+      , ("Test filtered splitting", splitTest True)
+      , ("Symbol data report", symbolReport)
+      , ("Symbol histogram", symbolReport)
       ]
 
 testFilteredSplitting :: IO ()
 testFilteredSplitting = putStrLn "Coming soon!"
 
-splitTest :: IO ()
-splitTest = do
+symbolHistogram :: IO ()
+symbolHistogram = do
+    let doFiltering = True
     putStrLn "Enter a divisor"
     divisor <- readLn
     putStrLn "Enter a threshold"
     t <- readLn
     tryBigTest (\img -> do
+        let symbols = reverse $ sortOn symbolWeight $ (if doFiltering then filter (filterFunc) else id) $ imageToSymbols img
+        sequence $ Prelude.map (putStrLn . show . symbolWeight) symbols
+        return ()
+        ) divisor t
+
+symbolReport :: IO ()
+symbolReport = do
+    putStrLn "Filtering? (True, False)"
+    doFiltering <- readLn
+    putStrLn "Enter a divisor"
+    divisor <- readLn
+    putStrLn "Enter a threshold"
+    t <- readLn
+    tryBigTest (\img -> do
+        let symbols = reverse $ sortOn symbolWeight $ (if doFiltering then filter (filterFunc) else id) $ imageToSymbols img
+        let symbolDataFns = [ show . fst . symbolDims
+                , show . snd . symbolDims
+                , show . fst . symbolOffset
+                , show . snd . symbolOffset
+                , show . symbolWeight
+                , show . (\x -> (fromIntegral $ numerator x)/(fromIntegral $ denominator x)) . symbolDensity
+                , show . not . filterFunc_long
+                , show . not . filterFunc_small
+                ]
+        let symbolDataResults = Prelude.map (\x-> intercalate ", " $ symbolDataFns <*> pure x) symbols
+
+        putStrLn "Height, Width, X, Y, Weight, Density, Too Long, Too Small"
+        sequence $ Prelude.map putStrLn symbolDataResults
+        return ()
+        ) divisor t
+
+
+smallSplitTest :: IO ()
+smallSplitTest = do
+    putStrLn "Enter a divisor"
+    divisor <- readLn
+    putStrLn "Enter a threshold"
+    t <- readLn
+    trySmallTest (\img -> do
         display img
         testSplitting img
         splitRecurse (getSymbolImages img)
@@ -50,6 +95,36 @@ splitTest = do
             if nextImageCount == 0 then return () else do
                 sequence $ Prelude.map display (take nextImageCount imgs)
                 splitRecurse (drop nextImageCount imgs)
+
+splitTest :: Bool -> IO ()
+splitTest doFiltering = do
+    putStrLn "Enter a divisor"
+    divisor <- readLn
+    putStrLn "Enter a threshold"
+    t <- readLn
+    tryBigTest (\img -> do
+        display img
+        testSplitting img
+        splitRecurse $ reverse $ sortOn symbolWeight $ (if doFiltering then filter (filterFunc) else id) $ imageToSymbols img
+        ) divisor t
+    where splitRecurse imgs = do
+            putStrLn "Show (n) next symbols: "
+            nextImageCount <- readLn
+            if nextImageCount == 0 then return () else do
+                sequence $ Prelude.map (display . symbolImage) (take nextImageCount imgs)
+                splitRecurse (drop nextImageCount imgs)
+
+filterFunc :: Symbol -> Bool
+filterFunc = do
+    long <- filterFunc_long
+    small <- filterFunc_small
+    return (long && small)
+
+filterFunc_long :: Symbol -> Bool
+filterFunc_long = (\x-> maximum x < 3*(minimum x) ). (<*>) [fst,snd] . pure . symbolDims
+
+filterFunc_small :: Symbol -> Bool
+filterFunc_small = ((<) 10) . symbolWeight
 
 displayOps :: [(String, IO ())] -> String
 displayOps = disp 0 
--- src/Orca/Reader/Layout.hs
+++ src/Orca/Reader/Layout.hs
@@ -1,4 +1,17 @@
-module Orca.Reader.Layout(Symbol, imageToSymbols, testSplitting, testSplitting2, testMap, getSymbolImages) where
+module Orca.Reader.Layout
+    ( Symbol
+    , symbolDims
+    , symbolOffset 
+    , symbolImage 
+    , symbolWeight 
+    , symbolDensity
+    , imageToSymbols
+    , testSplitting
+    , testSplitting2
+    , testMap
+    , getSymbolImages
+    ) where
+
 import Data.Maybe
 import Data.List as L
 import Data.Ratio
@@ -8,74 +21,14 @@ import Control.Monad.ST(runST)
 import qualified Data.Vector.Storable as V
 import qualified Data.Map as M
 
---stripsToSymbolData_cellFolding :: (Enum a, Ord a) => (a, [KeyStrip a], M.Map a SymbolData) -> YStrip -> (KeyStrip a, (a, [KeyStrip a], M.Map a SymbolData))
+-- REMOVE SHOW and debugging stuff. Reorganize. Use STData
 
-testSplitting2 :: IO ()
-testSplitting2 = do
-    let testSymbolData = stripsToSymbolData testStrips1
-    putStrLn $ show $ testSymbolData
-    putStrLn $ (++) "Symbols: " $ show $ length $ testSymbolData
-    putStrLn $ (++) "Strips: " $ show $ length $ concat $ map strips testSymbolData
-    putStrLn "Test ST"
-    putStrLn $ show $ stripsToSymbolData_cellFolding testSTData (KeyStrip 0 2 2)
-    putStrLn "Where it breaks"
-    putStrLn $ show $ mergeKeys 2 ([KeyStrip 2 3 0, KeyStrip 6 6 3], testMap2) 0
-
-testSplitting :: Image VS X Bit -> IO ()
---testSplitting = (\x -> x >> (return ())) . sequence . (map (putStrLn . show)) . (take 15) . splitImageToStrips
---testSplitting = (\x -> x >> (return ())) . putStrLn . concat . map show . imageToSymbols
-testSplitting = (\x -> x >> (return ())) . sequence . (map (putStrLn . show)) . L.sort . imageToSymbolData
---testSplitting = (\x -> x >> (return ())) . putStrLn . show . length . splitImageToStrips
-
-getSymbolImages :: Image VS X Bit -> [Image VS X Bit]
-getSymbolImages = map symbolDataToImage . reverse . L.sort . imageToSymbolData
-
-testMap :: ([KeyStrip Int], M.Map Int SymbolData)
-testMap = mergeKeys 2 ([(KeyStrip 2 8 3), (KeyStrip 9 12 2), (KeyStrip 14 14 5)],(M.fromList 
-                [ (2 :: Int, (initializeSymbolFromStrip (KeyStrip 1 3 0)))
-                , (3 :: Int, (initializeSymbolFromStrip (KeyStrip 2 5 1)))
-                ])) 3
-
-testSTData :: (Int, [KeyStrip Int], M.Map Int SymbolData)
-testSTData = (2 :: Int, keys, sd)
-    where ys = [(0, KeyStrip 1 2 1), (1, KeyStrip 4 7 1)]
-          sd = initializeSymbolFromStrip <$> M.fromList ys
-          keys = map (\(key, strp)-> swapKey strp key) ys
-
-testStrips1 :: [[YStrip]]
-testStrips1 = 
-    [ [(KeyStrip 4 5 0), (KeyStrip 1 2 0)]
-    , [(KeyStrip 6 6 1), (KeyStrip 2 3 1), (KeyStrip 0 0 1)]
-    , [(KeyStrip 4 6 2), (KeyStrip 0 2 2)]
-    , [(KeyStrip 2 2 3), (KeyStrip 0 0 3)]
-    ]
-
-testMap2 :: M.Map Int SymbolData
-testMap2 = M.fromList [(0,SymbolData {strips = [KeyStrip 2 3 1,KeyStrip 1 2 0], minX = 1, maxX = 3, minY = 0, maxY = 1, weight = 4}),(1,SymbolData {strips = [KeyStrip 4 5 0], minX = 4, maxX = 5, minY = 0, maxY = 0, weight = 2}),(2,SymbolData {strips = [KeyStrip 0 0 1], minX = 0, maxX = 0, minY = 1, maxY = 1, weight = 1}),(3,SymbolData {strips = [KeyStrip 6 6 1], minX = 6, maxX = 6, minY = 1, maxY = 1, weight = 1})]
-
--- REMOVE SHOW and debugging stuff in the last line . Show has also been added elsewhere
-stripsToSymbolData_cellFolding :: (Show a, Enum a, Ord a) => (a, [KeyStrip a], M.Map a SymbolData) -> YStrip -> (KeyStrip a, (a, [KeyStrip a], M.Map a SymbolData))
-stripsToSymbolData_cellFolding (a, [], dataMap) ystrip = (swapKey ystrip a, (succ a, [], M.insert a (initializeSymbolFromStrip ystrip) dataMap))
-stripsToSymbolData_cellFolding (a, keys@(keysHead:keysTail), dataMap) ystrip
-    | stripMax keysHead < stripMin ystrip = stripsToSymbolData_cellFolding (a, keysTail, dataMap) ystrip
-    | stripMax ystrip < stripMin keysHead = (swapKey ystrip a, (succ a, keys, M.insert a (initializeSymbolFromStrip ystrip) dataMap))
-    | otherwise = (swapKey ystrip headKey, (a, nextKeys, M.adjust (appendStripToSymbolData ystrip) headKey nextMap))
-    where toMerge = map stripKey $ takeWhile (checkStripCollision ystrip) keysTail
-          headKey = stripKey keysHead
-          (nextKeys, nextMap) = foldl (mergeKeys headKey) (drop (length toMerge) keys, dataMap) (if True || toMerge == [] then toMerge else (error $ "headkey:" ++ (show headKey) ++ "\nkeys" ++ (show keys) ++ "\nmap" ++ (show dataMap) ++  "\n - " ++ (show toMerge)) :: (Show a) =>[a])
-
--- Not safe when newKey == oldKey
-mergeKeys :: (Ord a) => a -> ([KeyStrip a], M.Map a SymbolData) -> a -> ([KeyStrip a], M.Map a SymbolData)
-mergeKeys newKey (keystrips, oldMap) oldKey = (correctedStrips, updatedMap)
-    where oldSymbolData = M.findWithDefault mempty oldKey oldMap
-          updatedMap = M.delete oldKey $ M.adjust (mappend oldSymbolData) newKey oldMap
-          correctedStrips = flip map keystrips $ \keystrip-> if (stripKey keystrip) == oldKey then swapKey keystrip newKey else keystrip
-
+{-- Public interface -}
 imageToSymbols :: Image VS X Bit -> [Symbol]
 imageToSymbols = map symbolFromData . stripsToSymbolData . splitImageToStrips
 
-imageToSymbolData :: Image VS X Bit -> [SymbolData]
-imageToSymbolData = stripsToSymbolData . splitImageToStrips
+
+{-Symbol Type-}
 
 data Symbol = Symbol
     { symbolDims :: (Int, Int)
@@ -153,6 +106,10 @@ checkStripCollision a b = min1 <= max2 && min2 <= max1
           max2 = stripMax b
         
 {-SymbolData basics-}
+
+imageToSymbolData :: Image VS X Bit -> [SymbolData]
+imageToSymbolData = stripsToSymbolData . splitImageToStrips
+
 data SymbolData = SymbolData
                     { strips :: [YStrip]
                     , minX :: Int
@@ -206,14 +163,10 @@ appendStripToSymbolData strip symbolData = symbolData
                                 , maxY = maxY1
                                 , weight = w1}) = symbolData
 
-convertVectorToStrips :: Int -> V.Vector Bool -> [YStrip]
-convertVectorToStrips y vec = maybe ss (flip (:) ss) result
-    where myFunc :: (Int, [YStrip], Maybe YStrip) -> Bool -> (Int, [YStrip], Maybe YStrip)
-          myFunc (x, ss, Nothing) True = (succ x, ss, Just (KeyStrip x x y))
-          myFunc (x, ss, Nothing) False = (succ x, ss, Nothing)
-          myFunc (x, ss, Just (KeyStrip xm _ _)) True = (succ x, ss, Just (KeyStrip xm x y))
-          myFunc (x, ss, Just lastStrip) False = (succ x, lastStrip:ss, Nothing)
-          (_, ss, result) = V.foldl myFunc (0, [], Nothing) vec
+{- Image -> Strips/STData -> SymbolData -> Symbol Conversion Process -}
+
+-- Use this later to make the type signatures more legible
+type STData a = (a, [KeyStrip a], M.Map a SymbolData)
 
 splitImageToStrips :: Image VS X Bit -> [[YStrip]]
 splitImageToStrips img = vectorToStrips 0 $ V.map isOn imgV
@@ -221,20 +174,21 @@ splitImageToStrips img = vectorToStrips 0 $ V.map isOn imgV
           imgV = toVector img
           vectorToStrips y vec
             | V.null vec = []
-            | otherwise = (convertVectorToStrips y fst_):(vectorToStrips (succ y) snd_)
+            | otherwise = (splitImageToStrips_convertVectorToStrips y fst_):(vectorToStrips (succ y) snd_)
             where (fst_,snd_) = V.splitAt w vec 
-
---
--- rowFolding (Ord a) => ([KeyStrip], Map a SymbolData) -> [YStrip] -> ([KeyStrip], Map a SymbolData)
---                            lastRow, map -> currentrow -> nextRow, updatedMap
--- cellFolding (Ord a) => ([KeyStrip], Map a SymbolData) -> YStrip -> ([KeyStrip], KeyStrip, Map a SymbolData)
---              keyStripsLeft, map -> testStrip -> (keyStripsLeft, thisKeyStrip, updatedMap)
-
--- Use this later to make the type signatures more legible
-type STData a = (a, [KeyStrip a], M.Map a SymbolData)
 
 stripsToSymbolData :: [[YStrip]] -> [SymbolData]
 stripsToSymbolData = M.elems . (\(_,_,x) -> x) . foldl stripsToSymbolData_rowFolding (0 :: Int, mempty, mempty)
+
+splitImageToStrips_convertVectorToStrips :: Int -> V.Vector Bool -> [YStrip]
+splitImageToStrips_convertVectorToStrips y vec = maybe ss (flip (:) ss) result
+    where myFunc :: (Int, [YStrip], Maybe YStrip) -> Bool -> (Int, [YStrip], Maybe YStrip)
+          myFunc (x, ss, Nothing) True = (succ x, ss, Just (KeyStrip x x y))
+          myFunc (x, ss, Nothing) False = (succ x, ss, Nothing)
+          myFunc (x, ss, Just (KeyStrip xm _ _)) True = (succ x, ss, Just (KeyStrip xm x y))
+          myFunc (x, ss, Just lastStrip) False = (succ x, lastStrip:ss, Nothing)
+          (_, ss, result) = V.foldl myFunc (0, [], Nothing) vec
+
 
 -- To optimize in the future: Redo stripsToData to work backwards, and remove "reverse" from this function
 stripsToSymbolData_rowFolding :: (Show a, Enum a, Ord a) => (a, [KeyStrip a], M.Map a SymbolData) -> [YStrip] -> (a, [KeyStrip a], M.Map a SymbolData)
@@ -242,3 +196,65 @@ stripsToSymbolData_rowFolding sData@(a, keys, dataMap) ystrip = (nextA, (reverse
                             where foldCollect :: (Show a, Enum a, Ord a) => ([KeyStrip a], (a, [KeyStrip a], M.Map a SymbolData)) -> YStrip -> ([KeyStrip a], (a, [KeyStrip a], M.Map a SymbolData))
                                   foldCollect (keyss, sData_) ystr = let (keystrip, newData) = stripsToSymbolData_cellFolding sData_ ystr in (keystrip:keyss, newData)
                                   (nextKeys, (nextA, _, nextMap)) = foldl foldCollect ([], sData) (reverse ystrip)
+
+stripsToSymbolData_cellFolding :: (Show a, Enum a, Ord a) => (a, [KeyStrip a], M.Map a SymbolData) -> YStrip -> (KeyStrip a, (a, [KeyStrip a], M.Map a SymbolData))
+stripsToSymbolData_cellFolding (a, [], dataMap) ystrip = (swapKey ystrip a, (succ a, [], M.insert a (initializeSymbolFromStrip ystrip) dataMap))
+stripsToSymbolData_cellFolding (a, keys@(keysHead:keysTail), dataMap) ystrip
+    | stripMax keysHead < stripMin ystrip = stripsToSymbolData_cellFolding (a, keysTail, dataMap) ystrip
+    | stripMax ystrip < stripMin keysHead = (swapKey ystrip a, (succ a, keys, M.insert a (initializeSymbolFromStrip ystrip) dataMap))
+    | otherwise = (swapKey ystrip headKey, (a, nextKeys, M.adjust (appendStripToSymbolData ystrip) headKey nextMap))
+    where toMerge = map stripKey $ takeWhile (checkStripCollision ystrip) keysTail
+          headKey = stripKey keysHead
+          (nextKeys, nextMap) = foldl (stripsToSymbolData_mergeKeys headKey) (drop (length toMerge) keys, dataMap) (if True || toMerge == [] then toMerge else (error $ "headkey:" ++ (show headKey) ++ "\nkeys" ++ (show keys) ++ "\nmap" ++ (show dataMap) ++  "\n - " ++ (show toMerge)) :: (Show a) =>[a])
+
+-- Not safe when newKey == oldKey
+stripsToSymbolData_mergeKeys :: (Ord a) => a -> ([KeyStrip a], M.Map a SymbolData) -> a -> ([KeyStrip a], M.Map a SymbolData)
+stripsToSymbolData_mergeKeys newKey (keystrips, oldMap) oldKey = (correctedStrips, updatedMap)
+    where oldSymbolData = M.findWithDefault mempty oldKey oldMap
+          updatedMap = M.delete oldKey $ M.adjust (mappend oldSymbolData) newKey oldMap
+          correctedStrips = flip map keystrips $ \keystrip-> if (stripKey keystrip) == oldKey then swapKey keystrip newKey else keystrip
+
+{-TEST STUFF-}
+
+testSplitting2 :: IO ()
+testSplitting2 = do
+    let testSymbolData = stripsToSymbolData testStrips1
+    putStrLn $ show $ testSymbolData
+    putStrLn $ (++) "Symbols: " $ show $ length $ testSymbolData
+    putStrLn $ (++) "Strips: " $ show $ length $ concat $ map strips testSymbolData
+    putStrLn "Test ST"
+    putStrLn $ show $ stripsToSymbolData_cellFolding testSTData (KeyStrip 0 2 2)
+    putStrLn "Where it breaks"
+    putStrLn $ show $ stripsToSymbolData_mergeKeys 2 ([KeyStrip 2 3 0, KeyStrip 6 6 3], testMap2) 0
+
+testSplitting :: Image VS X Bit -> IO ()
+--testSplitting = (\x -> x >> (return ())) . sequence . (map (putStrLn . show)) . (take 15) . splitImageToStrips
+--testSplitting = (\x -> x >> (return ())) . putStrLn . concat . map show . imageToSymbols
+testSplitting = (\x -> x >> (return ())) . sequence . (map (putStrLn . show)) . L.sort . imageToSymbolData
+--testSplitting = (\x -> x >> (return ())) . putStrLn . show . length . splitImageToStrips
+
+getSymbolImages :: Image VS X Bit -> [Image VS X Bit]
+getSymbolImages = map symbolDataToImage . reverse . L.sort . imageToSymbolData
+
+testMap :: ([KeyStrip Int], M.Map Int SymbolData)
+testMap = stripsToSymbolData_mergeKeys 2 ([(KeyStrip 2 8 3), (KeyStrip 9 12 2), (KeyStrip 14 14 5)],(M.fromList 
+                [ (2 :: Int, (initializeSymbolFromStrip (KeyStrip 1 3 0)))
+                , (3 :: Int, (initializeSymbolFromStrip (KeyStrip 2 5 1)))
+                ])) 3
+
+testSTData :: (Int, [KeyStrip Int], M.Map Int SymbolData)
+testSTData = (2 :: Int, keys, sd)
+    where ys = [(0, KeyStrip 1 2 1), (1, KeyStrip 4 7 1)]
+          sd = initializeSymbolFromStrip <$> M.fromList ys
+          keys = map (\(key, strp)-> swapKey strp key) ys
+
+testStrips1 :: [[YStrip]]
+testStrips1 = 
+    [ [(KeyStrip 4 5 0), (KeyStrip 1 2 0)]
+    , [(KeyStrip 6 6 1), (KeyStrip 2 3 1), (KeyStrip 0 0 1)]
+    , [(KeyStrip 4 6 2), (KeyStrip 0 2 2)]
+    , [(KeyStrip 2 2 3), (KeyStrip 0 0 3)]
+    ]
+
+testMap2 :: M.Map Int SymbolData
+testMap2 = M.fromList [(0,SymbolData {strips = [KeyStrip 2 3 1,KeyStrip 1 2 0], minX = 1, maxX = 3, minY = 0, maxY = 1, weight = 4}),(1,SymbolData {strips = [KeyStrip 4 5 0], minX = 4, maxX = 5, minY = 0, maxY = 0, weight = 2}),(2,SymbolData {strips = [KeyStrip 0 0 1], minX = 0, maxX = 0, minY = 1, maxY = 1, weight = 1}),(3,SymbolData {strips = [KeyStrip 6 6 1], minX = 6, maxX = 6, minY = 1, maxY = 1, weight = 1})]
